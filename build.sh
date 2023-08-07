bab9888101735b08962440f0af231e7bd6de6008
Simon Maurer <simon.maurer@unibe.ch>
2023-08-07 17:33:09 +0200

    add linux package

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-R -e 'devtools::document()'
-
-version=$(gawk 'match($0, /Version: ([0-9]+\.[0-9]+\.[0-9]+)/, a) {print a[1]}' DESCRIPTION)
-R CMD build .
-mv gar_$version.tar.gz release/.
