85c87988fadc8d3b0ee2f464b501b847c05503ae
Simon Maurer <simon.maurer@unibe.ch>
2023-08-24 15:40:37 +0200

    keep build folder after build and copy package into build folder

--- build_linux.sh
+++ build_linux.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf build
 mkdir -p build
 
 version=$(gawk 'match($0, /Version: ([0-9]+\.[0-9]+\.[0-9]+)/, a) {print a[1]}' DESCRIPTION)
@@ -8,6 +9,4 @@ package=gar_${version}_R_x86_64-pc-linux-gnu.tar.gz
 R -e 'devtools::document()'
 R CMD INSTALL -l build --build .
 
-mv ${package} release/.
-
-rm -rf build
+mv ${package} build/.
